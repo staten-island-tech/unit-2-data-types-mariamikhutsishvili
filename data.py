1f5cb55d26264fd936d5ed9674dfb482ee7b5a0d
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-02-06 13:59:34 -0500

    jhgjgj

---
+++ data.py
@@ -0,0 +1,38 @@
+""" #Data types, Numbers 1,2,3 etc. - toggle
+def add(x,y):
+    print(x + y)
+add(45,35)
+
+#Strings "a,b'c" - toggle
+def message(name):
+    print(name)
+message("Agnes")
+
+
+add("35","45")
+
+#booleans - toggle
+x = True
+
+def check(x):
+    if(x == True):
+        print("is logged in")
+    else:
+        print("please login")
+check(x)
+
+ """
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(values[0]) #brackets = p[osition in list start at 0]
+#parantheses = show the number 
+
+
+#strings
+    x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
