c244de85a2cc0bcaf9097224d5914003f54a625d
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-02-08 14:00:28 -0500

    ggggg

--- tips.py
+++ tips.py
@@ -1,1 +1,12 @@
-print("What is your total")
+#tip calculator
+
+x = int(input("Subtotal"))
+Service = input("How was the service")
+if Service == ("bad"): 
+    print(float(x))
+elif Service == ("okay"):
+    print(float(x*1.15))
+elif Service == ("good"):
+    print(float(x*1.2))
+elif Service == ("great"):
+    print(float(x*1.25))
