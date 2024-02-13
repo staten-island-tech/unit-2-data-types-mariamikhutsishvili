f54cdec773106d0120b06cc525bd3b4da90cc473
mariamikhutsishvili <158212154+mariamikhutsishvili@users.noreply.github.com>
2024-02-13 15:55:42 +0000

    hhbhbhbbbhbhbb

--- gcf2.py
+++ gcf2.py
@@ -3,7 +3,7 @@ def greatest_common_factor(x,y):
     for i in range(1, x+1):
         if x%i == 0 and y%i == 0:
             gcf.append(i)
-    return gcf[-1]
+    return gcf [-1]
 
 x = int(input("Number:"))
 y = int(input("Second Number:"))
