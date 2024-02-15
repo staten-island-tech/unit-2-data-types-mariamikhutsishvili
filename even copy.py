eef9b3f9df7744974f8f08379a627a23398c4c01
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-02-15 13:46:38 -0500

    dedfd

---
+++ even copy.py
@@ -0,0 +1,7 @@
+# determines wether a number is even or odd based on the remainder when divided by 2
+number_guess_game = int(input("even or odd"))
+
+if (() % 2) == 0:
+    print('even')
+else:
+    print('odd')
---
+++ ngg.py
@@ -0,0 +1,9 @@
+import random
+num = random.choice([1, 2, 3, 4, 5])
+print()
+x = int(input("Number:"))
+if (x) == (num):
+    print('correct')
+else:
+    print('try again')
+
