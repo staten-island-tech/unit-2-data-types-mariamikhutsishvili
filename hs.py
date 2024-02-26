d7d96d151ec0ddde4aff729940e307c17574a36e
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-02-26 14:00:55 -0500

    dffdgfgd

--- hs.py
+++ hs.py
@@ -1,1 +1,11 @@
-def last_name():
+
+y = int(input("first_initial of last name:"))
+z = int(input("homeroom:"))
+def first_initial(y):
+    if ((y) % 2) == 0:
+     print('even')
+    else:
+     print('odd')
+
+x = int(input("last_name"))
+
