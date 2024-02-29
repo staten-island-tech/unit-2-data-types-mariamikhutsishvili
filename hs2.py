b8eebb3fbfed83dc0d52d8043dd190bdf68dd076
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-02-29 14:00:29 -0500

    dfdfvdfvfv

---
+++ hs2.py
@@ -0,0 +1,11 @@
+y = (input("last name:")).upper()
+x = y[0]
+def first_initial(x):
+    if x == ("A", "B", "C", "D", "E", "F", "G"):
+     print('101')
+    elif x == ("H", "I", "J", "K", "L", "M", "N", "O", "P"):
+      print('102')
+    else:
+     print('103')
+
+first_initial(x)
