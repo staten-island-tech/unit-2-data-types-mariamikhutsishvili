53b27686fb63cc8201053c6c0290df7f971bb50f
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-03-11 13:59:39 -0400

    dfvrdg

--- question.py
+++ question.py
@@ -1,14 +1,15 @@
 accounts = [["1", "2", "3"], ["4", "5", "6", "7"], ["8", "9"]]
-y = [[int(i) for i in x] for x in accounts]
 
-print (x[0]) 
-def getMax():
-    x = sum([0])
-    b = sum(y[1])
-    a = sum(y[2])
-    if x > b and a: 
-        print(x)
-    if b > x and a: 
-        print(b)
-    if a > b and x: 
-        print(a)
+def max(accounts):
+    int_accounts = [[int(i) for i in x] for x in accounts]
+    largest = 0
+    for money in int_accounts:  
+        x = sum(money)
+        if x > largest: 
+            largest = x 
+        print (largest)
+
+def max(accounts):
+    int_accounts = [[int(i) for i in x] for x in accounts]
+    z = [sum(i) for i in int_accounts]
+    return(max(z))
